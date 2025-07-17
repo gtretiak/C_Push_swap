29911f1d41195240c707ec380fa5bc7d6f6d1c3e
Georgii Tretiakov <george22021988@gmail.com>
2025-07-17 13:27:28 +0100

    Update sort_many.c

--- Sources/sort_many.c
+++ Sources/sort_many.c
@@ -14,11 +14,11 @@
 
 void	ft_init_nodes(t_stack **a, t_stack **b)
 {
-	ft_set_index(a);
+	ft_set_index(a); // assigning index values to each node
 	ft_set_index(b);
-	ft_set_target(a, b);
-	ft_get_cost(a, b);
-	ft_set_cheapest(b);
+	ft_set_target(a, b); // identifying the best node in A to insert for each node in B 
+	ft_get_cost(a, b); // move nodes computation for each B node
+	ft_set_cheapest(b); // setting the cheapest node in B for moving
 }
 
 t_stack	*ft_find_smallest(t_stack *stack)
@@ -28,10 +28,10 @@ t_stack	*ft_find_smallest(t_stack *stack)
 
 	if (!stack)
 		return (NULL);
-	smallest = MAXINT;
-	while (stack)
+	smallest = MAXINT; // starting with INT_MAX
+	while (stack) // iterating through stack
 	{
-		if (stack->nb < smallest)
+		if (stack->nb < smallest) // reassigning smallest
 		{
 			smallest = stack->nb;
 			smallest_node = stack;
@@ -45,9 +45,9 @@ t_stack	*ft_return_cheapest(t_stack *stack)
 {
 	if (!stack)
 		return (NULL);
-	while (stack)
+	while (stack) // iterating through stack
 	{
-		if (stack->cheapest)
+		if (stack->cheapest) // if stack->cheapest == "true" is found
 			return (stack);
 		stack = stack->next;
 	}
@@ -66,7 +66,7 @@ void	ft_set_index(t_stack **stack)
 	median = ft_get_len(*stack) / 2;
 	curr = *stack;
 	while (curr)
-	{
+	{ // depending on the index, we determine which rotation direction is optimal
 		curr->index = i;
 		if (i <= median)
 			curr->above_median = true;
@@ -80,23 +80,23 @@ void	ft_set_index(t_stack **stack)
 void	ft_sort_many(t_stack **a, t_stack **b, int len)
 {
 	t_stack	*smallest;
-
+// first we push two elements in the stack b
 	if (!ft_check_if_sorted(*a))
 		ft_pb(a, b);
 	if (!ft_check_if_sorted(*a))
 		ft_pb(a, b);
-	while (len-- > 3 && !ft_check_if_sorted(*a))
-		ft_smart_pb(a, b);
+	while (len-- > 3 && !ft_check_if_sorted(*a)) // until we have three elements in A, we push number to B
+		ft_smart_pb(a, b); // refined way to push numbers to stack b
 	if (!ft_check_if_sorted(*a))
-		ft_sort_three(a);
-	while (*b)
+		ft_sort_three(a); // dedicated small-sorting algorithm
+	while (*b) // until we have something in B
 	{
-		ft_init_nodes(a, b);
-		ft_move_b_to_a(a, b);
+		ft_init_nodes(a, b); // we recalculate indexes, rotation directions, targets, costs, and cheapests
+		ft_move_b_to_a(a, b); // we make a movement
 	}
-	ft_set_index(a);
-	smallest = ft_find_smallest(*a);
-	if (smallest->above_median)
+	ft_set_index(a); // atthis point there´s nothing left in B, but even though numbers in A are in order, we might have the smallest number not on the top
+	smallest = ft_find_smallest(*a); // we find it
+	if (smallest->above_median) // determine rotation direction and rotate
 	{
 		while (*a != smallest)
 			ft_ra(a);
